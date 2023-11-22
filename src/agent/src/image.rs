8b1c0ee6993dafeee0599a2f09f756d5537aec46
ChengyuZhu6 <chengyu.zhu@intel.com>
2023-11-22 16:26:51 +0800

    agent/image: Enable image-rs crate to pull image inside guest
    With image-rs pull_image API, the downloaded container image layers
    will store at IMAGE_RS_WORK_DIR, and generated bundle dir with rootfs
    and config.json will be saved under CONTAINER_BASE/cid directory.
    
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>
    Co-authored-by: Arron Wang <arron.wang@intel.com>
    Co-authored-by: Jiang Liu <gerry@linux.alibaba.com>
    Co-authored-by: stevenhorsman <steven@uk.ibm.com>
    Co-authored-by: wllenyj <wllenyj@linux.alibaba.com>

--- src/agent/src/image.rs
+++ src/agent/src/image.rs
@@ -5,8 +5,19 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::sync::Arc;
+
 use anyhow::{anyhow, Result};
+use image_rs::image::ImageClient;
 use tokio::sync::Mutex;
+
+use crate::rpc::CONTAINER_BASE;
+
+const KATA_CC_IMAGE_WORK_DIR: &str = "/run/image/";
 
 #[rustfmt::skip]
 lazy_static! {
@@ -19,10 +30,18 @@ fn sl() -> slog::Logger {
 }
 
 #[derive(Clone)]
-pub struct ImageService {}
+pub struct ImageService {
+    image_client: Arc<Mutex<ImageClient>>,
+    images: Arc<Mutex<HashMap<String, String>>>,
+}
 impl ImageService {
     pub fn new() -> Self {
-        Self {}
+        env::set_var("CC_IMAGE_WORK_DIR", KATA_CC_IMAGE_WORK_DIR);
+
+        Self {
+            image_client: Arc::new(Mutex::new(ImageClient::default())),
+            images: Arc::new(Mutex::new(HashMap::new())),
+        }
     }
 
     /// Get the singleton instance of image service.
@@ -32,5 +51,48 @@ impl ImageService {
             .await
             .clone()
             .ok_or_else(|| anyhow!("image service is uninitialized"))
+    }
+
+    async fn add_image(&self, image: String, cid: String) {
+        self.images.lock().await.insert(image, cid);
+    }
+
+    pub async fn pull_image(
+        &self,
+        image: &str,
+        cid: &str,
+        image_metadata: &HashMap<String, String>,
+    ) -> Result<String> {
+        info!(sl(), "image metadata: {:?}", image_metadata);
+        let bundle_path = Path::new(CONTAINER_BASE).join(cid).join("images");
+        fs::create_dir_all(&bundle_path)?;
+        info!(sl(), "pull image {:?}, bundle path {:?}", cid, bundle_path);
+
+        let res = self
+            .image_client
+            .lock()
+            .await
+            .pull_image(image, &bundle_path, &None, &None)
+            .await;
+        match res {
+            Ok(image) => {
+                info!(
+                    sl(),
+                    "pull and unpack image {:?}, cid: {:?}, with image-rs succeed. ", image, cid
+                );
+            }
+            Err(e) => {
+                error!(
+                    sl(),
+                    "pull and unpack image {:?}, cid: {:?}, with image-rs failed with {:?}. ",
+                    image,
+                    cid,
+                    e.to_string()
+                );
+                return Err(e);
+            }
+        };
+        self.add_image(String::from(image), String::from(cid)).await;
+        Ok(format! {"{}/rootfs",bundle_path.display()})
     }
 }
